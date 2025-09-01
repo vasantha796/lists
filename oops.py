a412fa7f97571ba2d83c4284552f7e609a4d5f26
vasantha796 <vasanthapathlavath2121@gmail.com>
2025-09-01 11:34:44 +0530

    oops.py

---
+++ oops.py
@@ -0,0 +1,65 @@
+class calculator:
+    def add(self,a,b):
+        return a+b
+    def sub(self,a,b):
+        return a-b
+    def mul(self,a,b):
+        return a*b
+    def power(self,a,b):
+        return a**b
+    def div(self,a,b):
+        return a/b
+    def floor_div(self,a,b):
+        return a//b
+    def mod(self,a,b):
+        return a%b
+n1=calculator()
+n2=calculator()
+# n1
+print(n1.add(10,3))
+print(n1.sub(10,3))
+print(n1.mul(10,3))
+print(n1.div(10,3))
+print(n1.mod(10,3))
+print(n1.floor_div(10,3))
+print(n1.power(10,3))
+# n2
+print(n2.add(5,3))
+print(n2.sub(5,3))
+print(n2.mul(5,3))
+print(n2.div(5,3))
+print(n2.mod(5,3))
+print(n2.floor_div(5,3))
+print(n2.power(5,3))
+
+n1.model_num="S143"
+n1.made_in="india"
+n1.color="pink"
+n1.discount="10%"
+
+n2.model_num="B143"
+n2.made_in="PARIS"
+n2.color="black"
+n2.discount="20%"
+
+# ----n1----
+print(n1.model_num)
+print(n1.made_in)
+print(n1.color)
+print(n1.discount)
+# ----n2----
+print(n2.model_num)
+print(n2.made_in)
+print(n2.color)
+print(n2.discount)
+
+
+
+
+
+
+    
+
+
+
+        
